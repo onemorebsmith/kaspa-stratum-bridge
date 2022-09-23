015d3bb54fef0e90b1b4891b8a8348239b6f3ae6
Brandon Smith <smith.brandon.63@gmail.com>
2022-09-23 00:49:13 -0400

    hive

--- make_release.sh
+++ make_release.sh
@@ -2,8 +2,20 @@ CMD_PATH="../cmd/kaspabridge"
 rm -rf release
 mkdir -p release
 cd release
-mkdir -p ks_bridge_win64;env GOOS=windows GOARCH=amd64 go build -o ks_bridge_win64/ks_bridge.exe ${CMD_PATH};cp ${CMD_PATH}/config.yaml ks_bridge_win64/
-mkdir -p ks_bridge_linux;env GOOS=linux GOARCH=amd64 go build -o ks_bridge_linux/ks_bridge ${CMD_PATH};cp ${CMD_PATH}/config.yaml ks_bridge_linux/
+VERSION=1.1
+ARCHIVE="ks_bridge-${VERSION}"
+OUTFILE="ks_bridge"
+OUTDIR="ks_bridge"
 
-zip -r ks_bridge_win64.zip ks_bridge_win64
-tar -czvf ks_bridge_linux.tar.gz ks_bridge_linux
+# windows
+mkdir -p ${OUTDIR};env GOOS=windows GOARCH=amd64 go build -o ${OUTDIR}/${OUTFILE}.exe ${CMD_PATH};cp ${CMD_PATH}/config.yaml ${OUTDIR}/
+zip -r ${ARCHIVE}.zip ${OUTDIR}
+rm -rf ${OUTDIR}
+
+# linux
+mkdir -p ${OUTDIR};env GOOS=linux GOARCH=amd64 go build -o ${OUTDIR}/${OUTFILE} ${CMD_PATH};cp ${CMD_PATH}/config.yaml ${OUTDIR}/
+tar -czvf ${ARCHIVE}.tar.gz ${OUTDIR}
+
+# hive
+cp ../misc/hive/* ${OUTDIR}
+tar -czvf ${ARCHIVE}_hive.tar.gz ${OUTDIR}
